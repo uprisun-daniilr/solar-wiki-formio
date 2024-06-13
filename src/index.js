143c9f37ade04897f8a9e01b37d9928aece67987
Daniil R <daniilr@uprisun.tech>
2024-06-13 12:30:40 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import { GoogleMapProvider } from "./components/google-map.provider";
 import { UtilityProviderSelectComponent } from "./components/utility-select.component";
 
-(() => {
-  window.addEventListener("load", () => {
-    GoogleMapProvider.initialize();
+console.log(Formio.Components);
+window.addEventListener("load", () => {
+  console.log("Loaded");
+  GoogleMapProvider.initialize();
 
-    Formio.Providers.addProvider("address", "google", GoogleMapProvider);
+  Formio.Providers.addProvider("address", "google", GoogleMapProvider);
 
-    console.log(Formio.Components);
-    Formio.Components.addComponent(
-      "utilityproviderselect",
-      UtilityProviderSelectComponent
-    );
-    console.log(Formio.Components);
-  });
-})();
+  console.log(Formio.Components);
+  Formio.Components.addComponent(
+    "utilityproviderselect",
+    UtilityProviderSelectComponent
+  );
+  console.log(Formio.Components);
+});
