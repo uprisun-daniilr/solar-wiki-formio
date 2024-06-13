eb26980e566b3926cc595ab9fe75496dd805c962
Daniil R <daniilr@uprisun.tech>
2024-06-13 12:05:32 +0200

    Update

--- src/components/utility-select.component.js
+++ src/components/utility-select.component.js
@@ -23,7 +23,7 @@ export class UtilityProviderSelectComponent extends Formio.Components.components
       group: "basic",
       icon: "fa fa-list",
       weight: 70,
-      schema: CustomSelectComponent.schema(),
+      schema: UtilityProviderSelectComponent.schema(),
     };
   }
 
@@ -33,10 +33,39 @@ export class UtilityProviderSelectComponent extends Formio.Components.components
     const lat = form.getComponent("latitude").getValue();
     const lon = form.getComponent("longitude").getValue();
 
+    const api_key = this.component.api_key;
+
     if (lat && lon) {
-      url = `https://developer.nrel.gov/api/utility_rates/v3.json?api_key=DEMO_KEY&lat=${lat}&lon=${lon}`;
+      url = `https://developer.nrel.gov/api/utility_rates/v3.json?api_key=${api_key}&lat=${lat}&lon=${lon}`;
     }
 
     return super.loadItems(url, search, headers, options, method);
+  }
+
+  static initialize() {
+    UtilityProviderSelectComponent.editForm = (...args) => {
+      const editForm = Formio.Components.components.address.editForm(...args);
+      console.log(editForm.components);
+      // const tabs = editForm.components.find(({ type }) => type === "tabs");
+
+      // const provider = tabs.components.find(({ key }) => key === "provider");
+
+      // provider.components.push({
+      //   key: "data",
+      //   components: [
+      //     {
+      //       type: "textfield",
+      //       input: true,
+      //       label: "API Key",
+      //       key: "api_key",
+      //       weight: 20,
+      //       placeholder: "Enter your API key",
+      //       tooltip: "The API key for the data source",
+      //     },
+      //   ],
+      // });
+
+      return editForm;
+    };
   }
 }
